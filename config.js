327bf924831c717711119ec4d80910e20ed28ecd
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2023-07-27 11:02:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917866052212', 'Luffy', true],
+  ['917866052212', 'luffy', true], 
   [''] 
 ] //Number of owners
 
@@ -47,15 +47,15 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'Luffy'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.packname = 'Luffy┃ᴮᴼᵀ' 
+global.author = '@LuffyxDraken' 
+global.igfg = '▢ Follow on Instagram\nhttps://instagram.com/luffyxdraken?igshid=MzRlODBiNWFlZA== 
+global.dygp = 'https://chat.whatsapp.com/H3kVCHxtDNm8xSzuQJY68y'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'https://youtube.com/@LuffyxDraken'
+global.fgpyp = 'https://youtube.com/@LuffyxDraken'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 
 
